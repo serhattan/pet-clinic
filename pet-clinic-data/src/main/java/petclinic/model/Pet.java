97a98af1e8903cf034ca3ec03a9164e95e00bc96
Serhat Tan <94serhattan@gmail.com>
2019-03-25 20:14:26 +0300

    Added create and update pet. closed #31

--- pet-clinic-data/src/main/java/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/petclinic/model/Pet.java
@@ -16,9 +16,13 @@ import java.util.Set;
 public class Pet extends BaseEntity {
 
     @Builder
-    public Pet(Long id, String name) {
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
         super(id);
         this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        this.visits = visits;
     }
     @Column(name = "name")
     private String name;
